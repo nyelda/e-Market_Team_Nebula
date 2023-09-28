e732b158aff46f980419434619cd8326d7bb193b
nyelda <qdjsuyat1@tip.edu.ph>
2023-09-29 00:47:44 +0800

    updated design

--- components/Input.js
+++ components/Input.js
@@ -34,6 +34,7 @@ const styles = StyleSheet.create({
         fontStyle: 'italic',
         width: 'auto',
         height: 5,
+        color: '#EFD02C',
     },
     container: {
         width: '100%',
