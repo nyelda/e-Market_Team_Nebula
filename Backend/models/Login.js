02676de22d28eced19113c83509693e0bfe0f59b
junemel610 <meljune610@gmail.com>
2023-12-04 17:43:25 +0800

    Changed database model for contact numbers to be a string

--- Backend/models/Login.js
+++ Backend/models/Login.js
@@ -19,7 +19,7 @@ const UserSchema = new mongoose.Schema({
         required: true
     },
     contactNumber: {
-        type: Number,
+        type: String,
         required: true
     },
     program: {
